f8e2e090648f125c23aa22737d3afc59b09bcd24
Damiano Pellegrini <41305552+Alian7911@users.noreply.github.com>
2019-06-09 22:22:38 +0200

    Finished main page.

--- ts/index.ts
+++ ts/index.ts
@@ -19,7 +19,7 @@ module GithubLoader {
         $("#user-avatar").attr("src", userJson.avatar_url);
         $("#user-login").text(userJson.login);
         $("#user-name").text(userJson.name);
-        $("#user-followers").text(userJson.followers + " Follower" + (userJson.followers == 1 ? "s" : ""));
+        $("#user-followers").text(userJson.followers + " Follower" + (userJson.followers == 1 ? "" : "s"));
         $("#user-following").text(userJson.following + " Following");
         if (userJson.bio != null)
             $("#user-bio").text(userJson.bio).removeAttr("hidden");
@@ -46,19 +46,29 @@ module GithubLoader {
     //Load the current loaded user's repos
     export function loadUserRepos(): void {
         $.each(userReposJson, function (key, elem) {
-            $("<div/>")
-                .addClass("col-sm d-flex")
-                .append(
-                    $("<div/>")
-                        .addClass("sh-shadow d-flex flex-fill m-2 p-2 flex-column border border-tertiary")
-                        .append($("<h2/>").html(`<a href="${elem.html_url}">${elem.name}</a>`))
-                        .append($("<h5/>").text(elem.description).attr("hidden", "hidden"))
-                        .append($("<h4/>").text(elem.language))
-                )
-                .find("h5")
-                .removeAttr(elem.description == null ? "hidden" : "")
-                .end()
-                .appendTo($("#repo-container"));
+            console.log(elem);
+            // $("<div/>")
+            //     .addClass("col-sm d-flex")
+            //     .append(
+            //         $("<div/>")
+            //             .addClass("sh-shadow d-flex flex-fill m-2 p-2 flex-column border border-tertiary")
+            //             .append($("<h2/>").html(`<a href="${elem.html_url}">${elem.name}</a>`))
+            //             .append($("<h5/>").text(elem.description).attr("hidden", "hidden"))
+            //             .append($("<h4/>").text(elem.language))
+            //     )
+            //     .find("h5")
+            //     .removeAttr((elem.description != null) ? "hidden" : "")
+            //     .end()
+            //     .appendTo($("#repo-container"));
+
+            let col = $("<div/>").addClass("col-sm d-flex");
+            let shadowCont = $("<div/>").addClass("repo sh-shadow d-flex flex-fill m-2 p-2 flex-column");
+            let headerCont = $("<div/>").addClass("d-flex flex-row border-bottom border-glow");
+            let repoLink = $("<a/>").addClass("repo-link").attr("href", elem.html_url).text(elem.name.includes(".") ? elem.name.split(".").join("-") : elem.name);
+            let repoLang = $("<div/>").addClass("repo-language ml-auto").text(elem.language);
+            let repoDesc = $("<div/>").addClass("repo-desc").text(elem.description != null ? elem.description : "No description is provided.");
+            col.append(shadowCont.append(headerCont.append(repoLink).append(repoLang)).append(repoDesc));
+            col.appendTo($("#repo-container"));
         });
     }
 }
