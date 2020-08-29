c15981d49fcc79daff5bf7908152c37b03719c63
Damiano Pellegrini <damiano1.pellegrini@gmail.com>
2020-08-29 13:25:43 +0200

    Updated look and feel.

--- js/index.js
+++ js/index.js
@@ -1,62 +1,39 @@
 "use strict";
-var userJson;
-var userReposJson;
-var GithubLoader;
-(function (GithubLoader) {
-    function fetchUser() {
-        $.ajax({
-            url: 'https://api.github.com/users/Alian7911',
-            method: 'GET'
-        }).done(function (data) {
-            userJson = data;
-            loadUserInfo();
-            fetchUserRepos();
-        });
-    }
-    GithubLoader.fetchUser = fetchUser;
-    function loadUserInfo() {
-        $("#user-info").removeAttr("hidden");
-        $("#user-avatar").attr("src", userJson.avatar_url);
-        $("#user-login").text(userJson.login);
-        $("#user-name").text(userJson.name);
-        $("#user-followers").text(userJson.followers + " Follower" + (userJson.followers == 1 ? "" : "s"));
-        $("#user-following").text(userJson.following + " Following");
-        if (userJson.bio != null)
-            $("#user-bio").text(userJson.bio).removeAttr("hidden");
-        if (userJson.company != null)
-            $("#user-company").text(userJson.company).removeAttr("hidden");
-        if (userJson.location != null)
-            $("#user-location").text(userJson.location).removeAttr("hidden");
-        if (userJson.email != null)
-            $("#user-email").text(userJson.email).removeAttr("hidden");
-    }
-    GithubLoader.loadUserInfo = loadUserInfo;
-    function fetchUserRepos() {
-        $.ajax({
-            url: userJson.repos_url,
-            method: 'GET'
-        }).done(function (data) {
-            userReposJson = data;
-            loadUserRepos();
-        });
-    }
-    GithubLoader.fetchUserRepos = fetchUserRepos;
-    function loadUserRepos() {
-        $.each(userReposJson, function (key, elem) {
-            console.log(elem);
-            var col = $("<div/>").addClass("col-sm d-flex");
-            var shadowCont = $("<div/>").addClass("repo sh-shadow d-flex flex-fill m-2 p-2 flex-column");
-            var headerCont = $("<div/>").addClass("d-flex flex-row border-bottom border-glow");
-            var repoLink = $("<a/>").addClass("repo-link").attr("href", elem.html_url).text(elem.name.includes(".") ? elem.name.split(".").join("-") : elem.name);
-            var repoLang = $("<div/>").addClass("repo-language ml-auto").text(elem.language);
-            var repoDesc = $("<div/>").addClass("repo-desc").text(elem.description != null ? elem.description : "No description is provided.");
-            col.append(shadowCont.append(headerCont.append(repoLink).append(repoLang)).append(repoDesc));
-            col.appendTo($("#repo-container"));
-        });
-    }
-    GithubLoader.loadUserRepos = loadUserRepos;
-})(GithubLoader || (GithubLoader = {}));
+Object.defineProperty(exports, "__esModule", { value: true });
+var common_1 = require("./common");
 $(document).ready(function () {
-    GithubLoader.fetchUser();
+    $.ajax({
+        url: common_1.User_URL,
+        method: 'GET',
+    }).done(function (responseData) {
+        var user = responseData;
+        $('[data-role=real-name]').text(user.name);
+        $.ajax({
+            url: user.repos_url,
+            method: 'GET',
+        }).done(function (responseData) {
+            var userRepos = responseData;
+            var htmlReposObj = new Array();
+            userRepos.forEach(function (element) {
+                var obj = $('[data-role=repo-template]').clone();
+                obj.attr('data-role', 'repo');
+                obj.attr('data-id', element.id);
+                obj.removeAttr('hidden');
+                obj.find('[data-role=repo-title]').text(element.name);
+                obj.find('[data-role=repo-language]').text(element.language);
+                obj.find('[data-role=repo-description]').text(element.description);
+                obj.find('[data-role=repo-homepage]').text(element.homepage).attr('href', element.homepage);
+                obj.find('[data-role=repo-stars]').text(element.stargazers_count.toString() + " " + (element.stargazers_count != 1 ? 'stars' : 'star'));
+                obj.find('[data-role=repo-forks]').text(element.forks_count.toString() + " " + (element.forks_count != 1 ? 'forks' : 'fork'));
+                obj.find('[data-role=repo-button]').attr('href', element.html_url);
+                if (element.description == null || element.description == "")
+                    obj.find('[data-role=repo-description]').hide();
+                if (element.homepage == null || element.homepage == "")
+                    obj.find('[data-role=repo-homepage]').parent().hide();
+                htmlReposObj.push(obj);
+            });
+            $('[data-role=repo-container]').append(htmlReposObj);
+        });
+    });
 });
 //# sourceMappingURL=index.js.map
